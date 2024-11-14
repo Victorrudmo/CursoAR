ca3b47a54babcaa643762f18ffa6b440a30d740d
Victorrudmo <vruimor2002@g.educaan.es>
2024-11-14 08:20:02 +0100

    first commit

---
+++ Assets/Scripts/ARInteractionsManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARInteractionManager : MonoBehaviour
+{   
+    [SerializeField] private Camera aRCamera;
+    private ARRaycastManager aRRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private GameObject aRPointer;
+    private GameObject item3DModel;
+    public GameObject Item3DModel
+    
+    {
+        set
+        {
+            item3DModel = value;
+            item3DModel.transform.position = aRPointer.transform.position;
+            item3DModel.transform.parent = aRPointer.transform;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
